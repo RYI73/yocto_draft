4a0033be918a7b00524241746b0556de8e14a40f
Ruslan Valovyi <ruslanval@pro-esupplies.com>
2026-01-30 11:53:18 +0200

    Fixed script

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ case $1 in
         qemu-system-arm \
           -M virt -cpu cortex-a15 -m 256 \
           -kernel $BUILD_DIR_QEMUARM/tmp/deploy/images/qemuarm/zImage \
-          -drive if=none,file=$BUILD_DIR_QEMUARM/tmp/deploy/images/qemuarm/core-image-minimal-qemuarm-20260118122949.rootfs.ext4,format=raw,id=hd0 \
+          -drive if=none,file=$BUILD_DIR_QEMUARM/tmp/deploy/images/qemuarm/core-image-minimal-qemuarm.ext4,format=raw,id=hd0 \
           -device virtio-blk-device,drive=hd0 \
           -nographic \
           -serial mon:stdio \
